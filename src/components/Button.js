7708308b58229f04a46ba08989d225342a18b3ed
imma-fish-u <stankoagregatBel@gmail.com>
2022-08-30 13:19:05 +0300

    feat: add colors, fonts, and part of button functionality

---
+++ src/components/Button.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const button = {
+  cursor: 'pointer',
+};
+
+const Button = ({ children, type, onClick, className, style }) => {
+    return (
+        <button
+            type={type}
+            style={{ ...button, ...style }}
+            className={`${className}`}
+            onClick={onClick}>
+            {children}
+        </button>
+    );
+};
+
+export default Button;
