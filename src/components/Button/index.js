e8de8c92078bc4fe70da4932cfbc488e415464c3
bynikitachel <nikitavyatiorec@gmail.com>
2022-09-05 21:34:24 +0300

    updated button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React from 'react'
 
-const button = {
-    cursor: 'pointer',
-};
+const Arrow = ({ className, color = '' }) => (
+  <svg className={className} width="13" height="20" viewBox="0 0 13 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M12.5098 1.8701L10.7298 0.100098L0.839844 10.0001L10.7398 19.9001L12.5098 18.1301L4.37984 10.0001L12.5098 1.8701Z" fill={color} />
+  </svg>
+)
 
-const Button = ({ children, type, onClick, className, style }) => {
-    return (
-        <button
-            type={type}
-            style={{ ...button, ...style }}
-            className={`${className}`}
-            onClick={onClick}
-            >
-            {children}
-        </button>
-    );
-};
+const Button = ({ children, type = '', onClick, className = '', arrowLeft, arrowRight }) => {
+  return (
+    <button
+      className={`customButton customButton_${type} ${className}`}
+      onClick={onClick}
+    >
+      {arrowLeft && <Arrow className={'arrow'} />}
+      {children}
+      {arrowRight && <Arrow className={'arrow customButton__arrowRight'} />}
+    </button>
+  )
+}
 
-export default Button;
+export default Button
