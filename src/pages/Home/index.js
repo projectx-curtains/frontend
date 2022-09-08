77f4b28fb980e9cb74d4f29219bd8570a8ce9236
imma-fish-u <stankoagregatBel@gmail.com>
2022-09-08 18:15:09 +0300

    refactor: delete banner example

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,19 +4,6 @@ const Home = (props) => {
 
   return (
     <main className="home">
-      <section className="home__start">
-        <div className="home__start__banner">
-          <h1 className="home__start__banner__heading">
-            Стиль и уют в каждой детали 
-          </h1>
-          <p className="home__start__banner__description">
-            Создай свой дизайн-проект штор от идеи до реализации
-          </p>
-        </div>
-        <div className="home__start__categories">
-
-        </div>
-      </section>
       
     </main>
   )
