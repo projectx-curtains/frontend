c427c58fce48ee33e550cadad235af3afc3b1c52
bynikitachel <nikitavyatiorec@gmail.com>
2022-09-09 09:10:13 +0300

    refactored

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import OurWork from './pages/home/sections/OurWork';
-import Reviews from './pages/home/sections/Reviews';
-import NewProducts from "./pages/home/sections/NewProducts";
-import Popular from "./pages/home/sections/Popular/Popular";
-import {NewProductData} from './stuff/NewProductData';
+import OurWork from './pages/home/sections/OurWork'
+import Reviews from './pages/home/sections/Reviews'
+import NewProducts from './pages/home/sections/NewProducts'
+import Popular from './pages/home/sections/Popular/Popular'
+import { NewProductData } from './mockData/NewProductData'
+import WhyWeAre from './pages/home/sections/WhyWeAre'
 
-function App() {
-
-    const descriptionTextWork = '';
-    const descriptionTextReviews = '';
-    const user = '';
-    const location = '';
-    return (<div className="app">
-        <NewProducts newProductData={NewProductData}/>
-        <Popular popularData={NewProductData}/>
-        <OurWork descriptionText={descriptionTextWork}/>
-        <Reviews descriptionText={descriptionTextReviews} user={user} location={location}/>
-    </div>);
+function App()
+{
+  return (
+    <div className="app">
+      <NewProducts galleryItems={NewProductData} />
+      <Popular popularData={NewProductData} />
+      <OurWork />
+      <WhyWeAre />
+      <Reviews />
+    </div>)
 }
 
-export default App;
+export default App
