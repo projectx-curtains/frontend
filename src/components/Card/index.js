1e95bd039839475944ca238ef43c95bab9169a66
bynikitachel <nikitavyatiorec@gmail.com>
2022-09-19 11:17:25 +0300

    deleted unused code

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,20 +1,16 @@
-import React from "react";
+import React from 'react'
 
-const card = {
-    cursor: 'pointer',
-};
-
-const Card = ({image,index, setName, setPrice, onClick,}) => {
+const Card = ({ image, index, setName, setPrice, onClick }) => {
     return (
-        <div
-            onClick={onClick}
-            className='Card'
-        >
-            <img className='Card__img' src={image} alt='curtains'/>
-            <h1 className='Card__indexName'>{`${index} ${setName}`}</h1>
-            <p className='Card__priceAmount'>{setPrice}</p>
-        </div>
+      <div
+        onClick={onClick}
+        className="Card"
+      >
+          <img className="Card__img" src={image} alt="curtains" />
+          <h1 className="Card__indexName">{`${index} ${setName}`}</h1>
+          <p className="Card__priceAmount">{setPrice}</p>
+      </div>
     )
 }
 
-export default Card;
+export default Card
