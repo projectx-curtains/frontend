cac56a78e99c1c0be1a70017826359abd61ab8a9
bynikitachel <nikitavyatiorec@gmail.com>
2022-09-19 11:18:42 +0300

    refactored

--- src/components/WhyWeAreItem/index.js
+++ src/components/WhyWeAreItem/index.js
@@ -4,11 +4,9 @@ const WhyWeAreItem = ({ image, text }) => {
   return (
     <div className="why-we-are-item">
       <div className="why-we-are-item__image">
-        <img
-          src={require(`../../assets/icons/WhyWeAreIcons/${image}`)}
-          width="66pх"
-          height="66px"
-          alt="why we" />
+        <img className="icon"
+             src={require(`../../assets/icons/WhyWeAreIcons/${image}`)}
+             alt="why we" />
       </div>
       <div className="why-we-are-item__text">{text}</div>
     </div>
