12be3ef500709e72518e0aca1b6a37a7622282b3
bynikitachel <nikitavyatiorec@gmail.com>
2022-10-05 15:27:41 +0300

    fix-width-sections

--- src/pages/Home/OurWork/index.js
+++ src/pages/Home/OurWork/index.js
@@ -1,42 +1,44 @@
-import React from "react";
-import { useMediaQuery } from "react-responsive";
-import Button from "../../../components/Button";
-import Carousel from "../../../components/Carousel";
-import CarouselItem from "../../../components/Carousel/CarouselItem";
-import { CarouselSlider_mockData } from "../../../assets/data/carouselSlider_mockData";
-import Title from "../../../components/Title";
-import { app_mockData } from "../../../assets/data/app_mockData";
+import React from 'react'
+import { useMediaQuery } from 'react-responsive'
+import Button from '../../../components/Button'
+import Carousel from '../../../components/Carousel'
+import CarouselItem from '../../../components/Carousel/CarouselItem'
+import { CarouselSlider_mockData } from '../../../assets/data/carouselSlider_mockData'
+import Title from '../../../components/Title'
+import { app_mockData } from '../../../assets/data/app_mockData'
 
 const OurWork = () => {
-  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1200px)" });
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1200px)' })
 
   return (
     <section className="our-work">
-      <Title text="Наши работы" />
-      <Carousel>
-        {CarouselSlider_mockData.map((e, i, arr) => {
-          return (
-            <div key={i} className="work-content">
-              <div className="description-container">
-                <div className="description-container__description">
-                  {app_mockData.ourWork.descriptionText}
+      <div className="container">
+        <Title text="Наши работы" />
+        <Carousel>
+          {CarouselSlider_mockData.map((e, i, arr) => {
+            return (
+              <div key={i} className="work-content">
+                <div className="description-container">
+                  <div className="description-container__description">
+                    {app_mockData.ourWork.descriptionText}
+                  </div>
+                  <Button
+                    className={`button--${
+                      isTabletOrMobile ? 'secondary' : 'primary'
+                    } `}
+                    link="/consultation"
+                  >
+                    Консультация
+                  </Button>
+                  <CarouselItem currentIndex={i + 1} length={arr.length} />
                 </div>
-                <Button
-                  className={`button--${
-                    isTabletOrMobile ? "secondary" : "primary"
-                  } `}
-                  link="/consultation"
-                >
-                  Консультация
-                </Button>
-                <CarouselItem currentIndex={i + 1} length={arr.length} />
+                <img src={e.image} alt="our work" />
               </div>
-              <img src={e.image} alt="our work" />
-            </div>
-          );
-        })}
-      </Carousel>
+            )
+          })}
+        </Carousel>
+      </div>
     </section>
-  );
-};
-export default OurWork;
+  )
+}
+export default OurWork
--- src/pages/Home/Reviews/index.js
+++ src/pages/Home/Reviews/index.js
@@ -1,86 +1,88 @@
-import React from "react";
-import { useMediaQuery } from "react-responsive";
-import Carousel from "../../../components/Carousel";
-import CarouselItem from "../../../components/Carousel/CarouselItem";
-import Title from "../../../components/Title";
-import { CarouselSlider_mockData } from "../../../assets/data/carouselSlider_mockData";
-import { app_mockData } from "../../../assets/data/app_mockData";
+import React from 'react'
+import { useMediaQuery } from 'react-responsive'
+import Carousel from '../../../components/Carousel'
+import CarouselItem from '../../../components/Carousel/CarouselItem'
+import Title from '../../../components/Title'
+import { CarouselSlider_mockData } from '../../../assets/data/carouselSlider_mockData'
+import { app_mockData } from '../../../assets/data/app_mockData'
 
 const Reviews = () => {
-  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1200px)" });
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1200px)' })
 
   const handlerScrollUp = () => {
     window.scrollTo({
       top: 0,
       left: 0,
-      behavior: "smooth",
-    });
-  };
+      behavior: 'smooth',
+    })
+  }
 
   return (
     <section className="reviews">
-      <Title text="Отзывы" />
-      <Carousel>
-        {CarouselSlider_mockData.map((e, i, arr) => {
-          return (
-            <div key={i} className="reviews-content">
-              <div className="image-container">
-                <img
-                  className="image"
-                  src={e.image}
-                  alt="our work"
-                  width="650px"
-                  height="513px"
-                />
-                {isTabletOrMobile && (
-                  <div className="user">
-                    <div className="user__name">
-                      {app_mockData.reviews.user}
-                    </div>
-                    <div className="user__location">
-                      {app_mockData.reviews.location}
-                    </div>
-                  </div>
-                )}
-              </div>
-              <div className="description-container">
-                <div className="description">
+      <div className="container">
+        <Title text="Отзывы" />
+        <Carousel>
+          {CarouselSlider_mockData.map((e, i, arr) => {
+            return (
+              <div key={i} className="reviews-content">
+                <div className="image-container">
                   <img
-                    src={
-                      require("../../../assets/svg/format_quote.svg").default
-                    }
-                    alt="review"
+                    className="image"
+                    src={e.image}
+                    alt="our work"
+                    width="650px"
+                    height="513px"
                   />
-                  <CarouselItem currentIndex={i + 1} length={arr.length} />
+                  {isTabletOrMobile && (
+                    <div className="user">
+                      <div className="user__name">
+                        {app_mockData.reviews.user}
+                      </div>
+                      <div className="user__location">
+                        {app_mockData.reviews.location}
+                      </div>
+                    </div>
+                  )}
                 </div>
-                {!isTabletOrMobile && (
-                  <div className="user">
-                    <div className="user__name">
-                      {app_mockData.reviews.user}
-                    </div>
-                    <div className="user__location">
-                      {app_mockData.reviews.location}
-                    </div>
+                <div className="description-container">
+                  <div className="description">
+                    <img
+                      src={
+                        require('../../../assets/svg/format_quote.svg').default
+                      }
+                      alt="review"
+                    />
+                    <CarouselItem currentIndex={i + 1} length={arr.length} />
                   </div>
-                )}
-                <div className="description__text">
-                  {app_mockData.reviews.descriptionText}
+                  {!isTabletOrMobile && (
+                    <div className="user">
+                      <div className="user__name">
+                        {app_mockData.reviews.user}
+                      </div>
+                      <div className="user__location">
+                        {app_mockData.reviews.location}
+                      </div>
+                    </div>
+                  )}
+                  <div className="description__text">
+                    {app_mockData.reviews.descriptionText}
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
-      </Carousel>
-      {isTabletOrMobile && (
-        <div className="button-scroll-up">
-          <img
-            src={require("../../../assets/svg/UpArrow.svg").default}
-            alt="button-up"
-            onClick={handlerScrollUp}
-          />
-        </div>
-      )}
+            )
+          })}
+        </Carousel>
+        {isTabletOrMobile && (
+          <div className="button-scroll-up">
+            <img
+              src={require('../../../assets/svg/UpArrow.svg').default}
+              alt="button-up"
+              onClick={handlerScrollUp}
+            />
+          </div>
+        )}
+      </div>
     </section>
-  );
-};
-export default Reviews;
+  )
+}
+export default Reviews
