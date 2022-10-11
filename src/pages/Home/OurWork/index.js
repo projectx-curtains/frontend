ec4d7cd96c7303b1de708ef53c09658b0763f383
bynikitachel <nikitavyatiorec@gmail.com>
2022-10-11 13:29:56 +0300

    fix-sections-width

--- src/pages/Home/OurWork/index.js
+++ src/pages/Home/OurWork/index.js
@@ -1,41 +1,43 @@
-import React from "react";
-import LinkButton from "../../../components/LinkButton";
-import Carousel from "../../../components/Carousel";
-import CarouselItem from "../../../components/Carousel/CarouselItem";
-import Title from "../../../components/Title";
-import { ROUTES } from "../../../constants/routes";
-import { CarouselSlider_mockData } from "../../../assets/data/carouselSlider_mockData";
-import { app_mockData } from "../../../assets/data/app_mockData";
+import React from 'react'
+import LinkButton from '../../../components/LinkButton'
+import Carousel from '../../../components/Carousel'
+import CarouselItem from '../../../components/Carousel/CarouselItem'
+import Title from '../../../components/Title'
+import { ROUTES } from '../../../constants/routes'
+import { CarouselSlider_mockData } from '../../../assets/data/carouselSlider_mockData'
+import { app_mockData } from '../../../assets/data/app_mockData'
 
 const OurWork = () => {
   return (
     <section className="our-work" id="our-works">
-      <Title text="Наши работы" />
-      <Carousel>
-        {CarouselSlider_mockData.map((sliderItem, sliderIndex, sliderArr) => {
-          return (
-            <div key={sliderIndex} className="work-content">
-              <div className="description-container">
-                <div className="description-container__description">
-                  {app_mockData.ourWork.descriptionText}
+      <div className="container">
+        <Title text="Наши работы" />
+        <Carousel>
+          {CarouselSlider_mockData.map((sliderItem, sliderIndex, sliderArr) => {
+            return (
+              <div key={sliderIndex} className="work-content">
+                <div className="description-container">
+                  <div className="description-container__description">
+                    {app_mockData.ourWork.descriptionText}
+                  </div>
+                  <LinkButton
+                    className="description-container__button"
+                    link={ROUTES.consultation}
+                  >
+                    Консультация
+                  </LinkButton>
+                  <CarouselItem
+                    currentIndex={sliderIndex + 1}
+                    length={sliderArr.length}
+                  />
                 </div>
-                <LinkButton
-                  className="description-container__button"
-                  link={ROUTES.consultation}
-                >
-                  Консультация
-                </LinkButton>
-                <CarouselItem
-                  currentIndex={sliderIndex + 1}
-                  length={sliderArr.length}
-                />
+                <img src={sliderItem.image} alt="our work" />
               </div>
-              <img src={sliderItem.image} alt="our work" />
-            </div>
-          );
-        })}
-      </Carousel>
+            )
+          })}
+        </Carousel>
+      </div>
     </section>
-  );
-};
-export default OurWork;
+  )
+}
+export default OurWork
