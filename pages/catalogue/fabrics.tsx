91fa6e5a1d68a0dbce335f31c56ea33ccc83193f
KyzAlexander <kyzkasan@gmail.com>
2022-11-17 11:44:12 +0300

    add  CATALOGUE_METADATA

--- pages/catalogue/fabrics.tsx
+++ pages/catalogue/fabrics.tsx
@@ -3,15 +3,16 @@ import { HeadTags } from "@common/headTags";
 
 import MainLayout from "@layouts/mainLayout";
 import FabricsModule from "@modules/catalogue";
+import { CATALOGUE_METADATA } from "@modules/catalogue/mocks/meta.data";
 
 const FabricsPage: NextPage = () => {
   return (
     <>
       {/* TODO: add metadata for catalog page */}
-      {/* <HeadTags
+      <HeadTags
         title={CATALOGUE_METADATA.title}
         description={CATALOGUE_METADATA.description}
-      /> */}
+      />
       <MainLayout>
         <FabricsModule />
       </MainLayout>
---
+++ src/modules/catalogue/mocks/meta.data.tsx
@@ -0,0 +1,6 @@
+import { HeadData } from "@common/headTags/types";
+
+export const CATALOGUE_METADATA: HeadData = {
+  title: `Салон штор "Зашторим". Дизайн и пошив штор в Могилеве по честным ценам.`,
+  description: `Купить шторы, ткани, подушки, фурнитуру для придания комнате уюта в магазине "Зашторим", Могилев, Беларусь`,
+};
