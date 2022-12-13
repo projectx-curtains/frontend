9345a0309fb6c9aafc0628f509621cb903c77d12
KyzAlexander <kyzkasan@gmail.com>
2022-12-13 10:04:25 +0300

    fix LinkStructure

--- src/modules/catalogue/components/LinkStructure/components/index.tsx
+++ src/modules/catalogue/components/LinkStructure/components/index.tsx
@@ -1,31 +1,37 @@
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 import Stack from "@mui/material/Stack";
+import { Breadcrumbs } from "@mui/material";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import { ILinkStructureProps } from "../interfaces";
 
 import style from "../styles/index.module.scss";
 
 const LinkStructure: React.FC<ILinkStructureProps> = ({ currentTitle }) => {
-  return (
-    <Stack
-      className={style["link-structure"]}
-      direction="row"
-      spacing={1}
-      divider={<NavigateNextIcon fontSize="medium" sx={{ color: "#373B49" }} />}
+  const breadcrumbs = [
+    <Link
+      className={style["link-structure__home"]}
+      underline="none"
+      key="1"
+      color="#373b49"
+      href="/"
     >
-      <Link
-        className={style["link-structure__home"]}
-        underline="none"
-        key="1"
-        color="#373b49"
-        href="/"
+      Главная
+    </Link>,
+    <Typography key="2" color="#6a7082">
+      {currentTitle}
+    </Typography>,
+  ];
+  return (
+    <Stack className={style["link-structure"]} spacing={1}>
+      <Breadcrumbs
+        aria-label="breadcrumb"
+        separator={
+          <NavigateNextIcon fontSize="medium" sx={{ color: "#373B49" }} />
+        }
       >
-        Главная
-      </Link>
-      <Typography key="2" color="#6a7082">
-        {currentTitle}
-      </Typography>
+        {breadcrumbs}
+      </Breadcrumbs>
     </Stack>
   );
 };
