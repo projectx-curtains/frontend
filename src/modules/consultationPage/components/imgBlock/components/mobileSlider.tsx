ce9a968d358119ceb53462c44a2b71e5df020ba5
KyzAlexander <kyzkasan@gmail.com>
2023-04-03 11:01:15 +0300

    correct slider in mobile

--- src/modules/consultationPage/components/imgBlock/components/mobileSlider.tsx
+++ src/modules/consultationPage/components/imgBlock/components/mobileSlider.tsx
@@ -6,42 +6,15 @@ import style from "../styles/index.module.scss";
 
 const MobileSlider: React.FC = () => {
   return (
-    // <div className={style["mobile-wrapper-img"]}>
-    //   <Slider
-    //     speed={500}
-    //     // rows={1}
-    //     // slidesPerRow={1}
-    //     // infinite={true}
-    //     centerMode={true}
-    //     slidesToShow={1}
-    //     slidesToScroll={1}>
-    //     {IMGS_DATA.map((img) => {
-    //       return (
-    //         <div
-    //           key={img.id}
-    //           className={style["mobile-wrapper-img__wrapper-img"]}>
-    //           <Image
-    //             key={img.id}
-    //             // className={style["wrapper-img__img"]}
-    //             src={require(`@assets/img/ConsultationPage/${img.image}`)}
-    //             alt="curtains"
-    //             width={img.widthMobile}
-    //             height={img.heightMobile}
-    //           />
-    //           <p className={style["mobile-wrapper-img__text-img"]}>
-    //             {img.text}
-    //           </p>
-    //         </div>
-    //       );
-    //     })}
-    //   </Slider>
-    // </div>
     <div className={style["mobile-wrapper-img"]}>
       <Slider
-        className={style["slider"]}
+        // className={style["slider"]}
         centerMode={true}
         infinite={false}
         speed={500}
+        // arrows={false}
+        // vertical={true}
+        // verticalSwiping={true}
         slidesToShow={1}
         slidesToScroll={1}>
         {IMGS_DATA.map((img) => {
